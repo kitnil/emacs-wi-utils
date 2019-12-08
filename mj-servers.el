de33b9560f9058e1a370298c4fb3f15f219dbcda
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-08 20:32:10 +0300

    mj-servers: Update.

--- mj-servers.el
+++ mj-servers.el
@@ -4,19 +4,46 @@
   `((id   . ,server)
     (name . ,server)))
 
-(defun mj-servers-get-entries ()
-  (mapcar 'mj-server->entry
-          '("web15.intr" "web16.intr" "web17.intr" "web18.intr" "web19.intr" "web20.intr"
-            "web21.intr" "web22.intr" "web23.intr" "web25.intr" "web26.intr" "web27.intr" "web28.intr" "web29.intr" "web30.intr"
-            "web31.intr" "web32.intr" "web33.intr" "web34.intr" "web35.intr" "web36.intr" "web37.intr"
+;; Code from counsel-tramp.
+(defun mj-hosts--candidates (&optional file)
+  "Collect candidates for counsel-tramp from FILE."
+  (let ((source (split-string
+                 (with-temp-buffer
+                   (insert-file-contents (or file "~/.ssh/config"))
+                   (buffer-string))
+                 "\n"))
+        (hosts (if file '() counsel-tramp-custom-connections)))
+    (dolist (host source)
+      (when (string-match "[H\\|h]ost +\\(.+?\\)$" host)
+	(setq host (match-string 1 host))
+	(if (string-match "[ \t\n\r]+\\'" host)
+	    (replace-match "" t t host))
+	(if (string-match "\\`[ \t\n\r]+" host)
+	    (replace-match "" t t host))
+        (unless (string= host "*")
+	  (if (string-match "[ ]+" host)
+	      (let ((result (split-string host " ")))
+		(while result
+		  (push
+		   (concat "/" tramp-default-method ":" (car result) ":")
+		   hosts)
+		  (push
+		   (concat "/ssh:" (car result) "|sudo:root@" (car result) ":/")
+		   hosts)
+		  (pop result)))
+	    (push
+	     (concat host)
+	     hosts))))
+      (when (string-match "Include +\\(.+\\)$" host)
+        (setq include-file (match-string 1 host))
+        (when (not (file-name-absolute-p include-file))
+          (setq include-file (concat (file-name-as-directory "~/.ssh") include-file)))
+        (when (file-exists-p include-file)
+          (setq hosts (append hosts (counsel-tramp--candidates include-file))))))
+    (reverse hosts)))
 
-            "kvm1.intr" "kvm10.intr" "kvm11.intr" "kvm12.intr"
-            "kvm13.intr" "kvm14.intr" "kvm15.intr" "kvm16.intr" "kvm17.intr"
-            "kvm19.intr" "kvm2.intr" "kvm20.intr" "kvm21.intr" "kvm22.intr"
-            "kvm23.intr" "kvm24.intr" "kvm25.intr" "kvm26.intr" "kvm27.intr"
-            "kvm28.intr" "kvm29.intr" "kvm30.intr" "kvm31.intr" "kvm32.intr"
-            "kvm33.intr" "kvm34.intr" "kvm35.intr" "kvm36.intr" "kvm37.intr"
-            "kvm5.intr" "kvm6.intr" "kvm9.intr")))
+(defun mj-servers-get-entries ()
+  (mapcar 'mj-server->entry (mj-hosts--candidates)))
 
 (defvar mj-servers-list-default-hint
   '(("\\[mj-servers-list-tramp]") " open TRAMP session;\n"
